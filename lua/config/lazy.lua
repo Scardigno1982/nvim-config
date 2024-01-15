1409b2fc07350291aa3fa94fae3945010a094cb3
sergio <sergioscardigno82@gmail.com>
2024-01-15 16:36:11 -0300

    Commet

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,6 @@ require("lazy").setup({
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -29,6 +28,7 @@ require("lazy").setup({
   -- install = { colorscheme = { "dracula", "habamax" } },
   theme = 'dracula-nvim',
 
+  
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
